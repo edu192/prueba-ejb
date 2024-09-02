27277fe7cc6d208f037da83f6b1b8aba0c28fbf0
edu <89234306+edu455@users.noreply.github.com>
2024-09-02 16:23:54 -0500

    Create alumno controller and register resource routes

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::resource('alumno', \App\Http\Controllers\AlumnoController::class);
 
 require __DIR__.'/auth.php';
