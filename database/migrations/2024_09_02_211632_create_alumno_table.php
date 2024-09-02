c6c519e4c60a12584aaf68946879d010ad1a0421
edu <89234306+edu455@users.noreply.github.com>
2024-09-02 16:19:54 -0500

    initial commit

---
+++ database/migrations/2024_09_02_211632_create_alumno_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('alumno', function (Blueprint $table) {
+            $table->id();
+            $table->string('apellido_paterno',20);
+            $table->string('apellido_materno',20);
+            $table->string('nombres',30);
+            $table->string('direccion',60);
+            $table->dateTime('fecha_nacimiento');
+            $table->integer('edad');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('alumno');
+    }
+};
